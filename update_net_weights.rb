d16343b9d7db46f1666267709997bd3a5bb2a186
newrushbolt <mmmmadak@yandex.ru>
2016-08-24 15:13:19 +0300

    Updated data paths

--- update_net_weights.rb
+++ update_net_weights.rb
@@ -11,7 +11,7 @@ peer_db=SQLite3::Database.new(peer_db_file)
 cnt=0
 begin
 	#Getting all id in the system
-	req="select webrtc_id from #{peer_state_table} order by last_online desc;"
+	req="select webrtc_id, from #{peer_state_table} order by last_online desc;"
 	# puts req
 	res=peer_db.execute(req)
 	# puts res.to_s
@@ -24,13 +24,13 @@ peer_list.each do |peer_id|
 	#get peer info
 	#puts peer_id
 	begin
-		req="select * from #{peer_state_table} where webrtc_id = \"#{peer_id[0]}\";"
-		puts req
+		req="select webrtc_id, from #{peer_state_table} where webrtc_id = \"#{peer_id[0]}\";"
+		#puts req
 		res=peer_db.execute(req)
 	rescue => e
 		puts "Error while getting peer #{peer_id}"
 		puts e.to_s
 	end
-	peer_info=res
+	peer_info[]=res
 	raise 'fuck'
 end
