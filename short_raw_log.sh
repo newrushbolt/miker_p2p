e0fdb207243fa47c220693e88148921b9d340d05
newrushbolt <mihailov.s@infra1.gg>
2016-08-29 15:44:38 +0300

    modified:   config.rb 	new file:   short_raw_log.sh

---
+++ short_raw_log.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cnt=$1
+echo '{"Logs":[' > data/raw_log_$cnt
+head data/raw_log -n $cnt >>data/raw_log_$cnt
+sed -i 's/\}$/},/' data/raw_log_$cnt
+sed -i 's/.$//' data/raw_log_$cnt
+echo ']}' >> data/raw_log_$cnt
