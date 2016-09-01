95627e848032c584ab3b0182e9e5ce0d3e5ab884
newrushbolt <mmmmadak@yandex.ru>
2016-09-02 00:58:42 +0300

    unrelevant cleanup

--- ripe_inetnums.cron.rb
+++ ripe_inetnums.cron.rb
@@ -1,14 +1,17 @@
 require "#{Dir.pwd}/config.rb"
 require 'rubygems'
 require 'mysql2'
-require 'whois'
 require 'ipaddr'
 require 'mysql2'
 
 $p2p_db_client=Mysql2::Client.new(:host => $p2p_db_host, :database => $p2p_db, :username => $p2p_db_user, :password => $p2p_db_pass)
-#$p2p_db_inetnums_tabl
-def prefix2netmask(prefix)
-	
+
+def get_apnic_latest_db
+
+end
+
+def parse_apnic_latest_db
+
 end
 
 def get_ripe_latest_db
@@ -26,7 +29,6 @@ def parse_ripe_latest_db
 	data.split("\n").each do |unit|
 		line={}
 		ip=IPAddr.new(unit.split(",")[0])
-#		puts unit.split(",")[1]
 		begin
 			line["asn"]=unit.split(",")[1].gsub(/\D/,"")
 		rescue => e
@@ -34,7 +36,6 @@ def parse_ripe_latest_db
 			puts unit
 			exit
 		end
-#		puts line["asn"]
 		line["netmask"]=ip.inspect.gsub(/^\#.*\//,"").delete(">")
 		line["network"]=ip.to_s
 		inetnums.push(line)
