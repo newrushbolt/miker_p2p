0608c91d8313e5e1103de5135da8271131b79624
root <root@coder5>
2016-09-04 17:05:42 +0300

    Whois cron update

--- inetnums.cron.rb
+++ inetnums.cron.rb
@@ -7,33 +7,49 @@ require 'mysql2'
 $p2p_db_client=Mysql2::Client.new(:host => $p2p_db_host, :database => $p2p_db, :username => $p2p_db_user, :password => $p2p_db_pass)
 $inetnums=[]
 
-def get_db(url,rr_name)
-	if url.ends_with?('.gz')
-		req="curl #{url} -o #{Dir.pwd}/data/#{rr_name}.db.gz;gzip -d #{Dir.pwd}/data/#{rr_name}.db.gz;grep -e '^route:|^origin:'>#{Dir.pwd}/data/#{rr_name}.routes"
+def get_db(url,rr_name,db_filename)
+	if ! File.exists?(db_filename)
+	if url.end_with?('.gz')
+		req="curl #{url} -o #{db_filename}.gz;gzip -df #{db_filename}.gz"
 	else
-		req="curl #{url} -o #{Dir.pwd}/data/#{rr_name}.db;grep -e '^route:|^origin:'>#{Dir.pwd}/data/#{rr_name}.routes"
+		req="curl #{url} -o #{db_filename}"
 	end
-	res=system(req)
 	return res
+	else
+	res=true
+	return res
+	end
+
 end
 
-def parse_db(filename)
+def parse_db(db_filename)
+	inetnums=[]
 	begin
-		data_raw=File.read(filename)
+		data_raw=File.read(db_filename).encode('utf-8', :invalid => :replace, :undef => :replace)
 	rescue => e
 		puts e.to_s
 	end
-	data=data_raw.gsub("\norigin:",",origin:").gsub("origin:","").gsub("route:","").delete(" ")
-	data.split("\n").each do |unit|
-		line={}
-		ip=IPAddr.new(unit.split(",")[0])
+	data_raw.split("\n\n").each do |raw_object|
+	    if raw_object.start_with?("route:")
 		begin
-			line["asn"]=unit.split(",")[1].gsub(/\D/,"")
+
+		raw_ip=raw_object.match(/^route:.*$/).to_s.gsub(/^route:[\ ]*/,"").delete(" ")
+		raw_asn=raw_object.match(/^origin:.*$/).to_s.gsub(/^origin:[\ ]*(AS|as|As|aS)/,"").delete(" ").to_i
+		rescue => e
+			puts 'error while converting object data'
+			puts raw_object
+			puts e.to_s
+			raw_ip=nil
+			raw_asn=nil
+		end
+
+		line={}
+		begin
+			ip=IPAddr.new(raw_ip)
 		rescue => e
 			puts e.to_s
-			puts unit
-			exit
 		end
+		line["asn"]=unit.split(",")[1].gsub(/\D/,"")
 		line["netmask"]=ip.inspect.gsub(/^\#.*\//,"").delete(">")
 		line["network"]=ip.to_s
 		inetnums.push(line)
@@ -44,12 +60,18 @@ end
 $rr_urls.each do |rr_url|
 	rr_name=rr_url.gsub("ftp://ftp.","").gsub(/\.net.*/,"")
 	puts rr_name
-	if get_db(rr_url,rr_name)==true
-		$inetnums=$inetnums | parse_db("#{Dir.pwd}/data/#{rr_name}.db")
+	db_filename="#{Dir.pwd}/data/#{rr_name}.db"
+	routes_filename="#{Dir.pwd}/data/#{rr_name}.routes"
+	if get_db(rr_url,rr_name,db_filename)==true
+		$inetnums=$inetnums | parse_db(db_filename)
 	else
 		puts 'failed to get db'
 	end
+exit
 end
+
+puts inetnums.lenght
+exit
 
 $inetnums.each do |inetnum|
 	begin
