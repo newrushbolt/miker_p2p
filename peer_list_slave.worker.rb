077c809f7c2b2a0f651abeab48720e481b580c0e
svmikhailov <svmikhailov@yamoney.ru>
2017-07-31 21:06:31 +0300

    nats migr

--- peer_list_slave.worker.rb
+++ peer_list_slave.worker.rb
@@ -22,21 +22,23 @@ class Peer_list_slave_worker < Common_worker
 
 	public
 	def run
-		while true
-			@bunny_workers["peer_lists_tasks"].subscribe(:block => true,:manual_ack => true) do |delivery_info, properties, body|
-				$err_logger.debug "Got task for peer #{body}"
-				resp=make_peer_list(body.to_s)
-				$err_logger.debug "Got peer_list: #{resp}"
-				if resp!=nil
-					add_peer_list_to_db(resp)
+		NATS.start(:servers => $nats_servers) do
+			while true
+				NATS.subscribe("peer_lists_tasks") do |body|
+					$err_logger.debug "Got task for peer #{body}"
+					resp=make_peer_list(body.to_s)
+					$err_logger.debug "Got peer_list: #{resp}"
+					if resp!=nil
+						add_peer_list_to_db(resp)
+					end
 				end
-				@rabbit_channel.acknowledge(delivery_info.delivery_tag, false)
 			end
 		end
 	end
 
 end
 
+require 'nats/client'
 while true
 	begin
 		current_worker=nil
@@ -45,7 +47,7 @@ while true
 		$err_logger.info e.to_s
 	end
 	begin
-		current_worker=Peer_list_slave_worker.new(worker_id: ARGV[0],worker_log_level: ARGV[1],bunny_queues: ["peer_lists_tasks"])
+		current_worker=Peer_list_slave_worker.new(worker_id: ARGV[0],worker_log_level: ARGV[1])
 		current_worker.run
 	rescue => e
 		$err_logger.error "Error in main module,restarting the class"
