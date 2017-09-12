ad77733c9d0a3ff75322af3bd747858c6f7a453a
Sergey <mihailov.s@outlook.com>
2017-09-12 17:48:21 +0300

    some ddl debug1

--- init/p2p.sql
+++ init/p2p.sql
@@ -1,10 +1,17 @@
+DROP TABLE IF EXISTS channels_slots CASCADE;
+CREATE TABLE channels_slots (
+  channel_id varchar(45) UNIQUE PRIMARY KEY NOT NULL,
+  slot_limit int NOT NULL
+);
+
 DROP TABLE IF EXISTS peers CASCADE;
 CREATE TABLE peers (
   conn_id varchar(45) UNIQUE NOT NULL,
   channel_id varchar(45) NOT NULL,
   gg_id varchar(45) DEFAULT NULL,
   ip inet NOT NULL,
-  CONSTRAINT connid_channel PRIMARY KEY (conn_id,channel_id)
+  CONSTRAINT peers_connid_channel PRIMARY KEY (conn_id,channel_id),
+  CONSTRAINT peers_channelid_slot FOREIGN KEY channel_id REFERENCES channels_slots (channel_id)
 );
 
 DROP TABLE IF EXISTS networks CASCADE;
@@ -14,14 +21,14 @@ CREATE TABLE networks (
   country varchar(45) DEFAULT NULL,
   region varchar(45) DEFAULT NULL,
   city varchar(45) DEFAULT NULL,
-  CONSTRAINT network_asn UNIQUE (network,asn)
+  CONSTRAINT networks_network_asn UNIQUE (network,asn)
 );
 
 DROP TABLE IF EXISTS peers_updates CASCADE;
 CREATE TABLE peers_updates (
   conn_id varchar(45) UNIQUE PRIMARY KEY NOT NULL,
   last_update timestamp NOT NULL,
-  CONSTRAINT peer_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id)
+  CONSTRAINT peers_updates_peer_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id)
 );  
 
 DROP TABLE IF EXISTS peers_lists CASCADE;
@@ -29,14 +36,8 @@ CREATE TABLE peers_lists (
   conn_id varchar(45) NOT NULL,
   ts timestamp NOT NULL,
   peers_list text NOT NULL,
-  CONSTRAINT connid_ts PRIMARY KEY (conn_id,ts),
-  CONSTRAINT peer_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id)
-);
-
-DROP TABLE IF EXISTS channels_slots CASCADE;
-CREATE TABLE channels_slots (
-  channel_id varchar(45) UNIQUE PRIMARY KEY NOT NULL,
-  slot_limit int NOT NULL
+  CONSTRAINT peers_lists_connid_ts PRIMARY KEY (conn_id,ts),
+  CONSTRAINT peers_lists_peer_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id)
 );
 
 DROP TABLE IF EXISTS peers_good CASCADE;
@@ -46,9 +47,9 @@ CREATE TABLE peers_good (
   ts int NOT NULL,
   bytes int NOT NULL,
   ltime int DEFAULT NULL,
-  CONSTRAINT seed_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id),
-  CONSTRAINT peer_conn_id FOREIGN KEY (peer_conn_id) REFERENCES peers (conn_id),
-  CONSTRAINT seed_peer_ts UNIQUE (conn_id,peer_conn_id,ts)
+  CONSTRAINT peers_good_seed_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id),
+  CONSTRAINT peers_good_peer_conn_id FOREIGN KEY (peer_conn_id) REFERENCES peers (conn_id),
+  CONSTRAINT peers_good_seed_peer_ts UNIQUE (conn_id,peer_conn_id,ts)
 );
 
 DROP TABLE IF EXISTS ip_good CASCADE;
@@ -58,7 +59,7 @@ CREATE TABLE ip_good (
   ts int NOT NULL,
   bytes int NOT NULL,
   ltime int DEFAULT NULL,
-  CONSTRAINT seed_peer_ts UNIQUE (ip,peer_ip,ts)
+  CONSTRAINT ip_good_seed_peer_ts UNIQUE (ip,peer_ip,ts)
 );
 
 DROP TABLE IF EXISTS peers_bad CASCADE;
@@ -66,9 +67,9 @@ CREATE TABLE peers_bad (
   conn_id varchar(45) PRIMARY KEY NOT NULL,
   peer_conn_id varchar(45) NOT NULL,
   ts int NOT NULL,
-  CONSTRAINT seed_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id),
-  CONSTRAINT peer_conn_id FOREIGN KEY (peer_conn_id) REFERENCES peers (conn_id),
-  CONSTRAINT seed_peer_ts UNIQUE (conn_id,peer_conn_id,ts)
+  CONSTRAINT peers_bad_seed_conn_id FOREIGN KEY (conn_id) REFERENCES peers (conn_id),
+  CONSTRAINT peers_bad_peer_conn_id FOREIGN KEY (peer_conn_id) REFERENCES peers (conn_id),
+  CONSTRAINT peers_bad_seed_peer_ts UNIQUE (conn_id,peer_conn_id,ts)
 );
 
 DROP TABLE IF EXISTS ip_bad CASCADE;
@@ -76,37 +77,37 @@ CREATE TABLE ip_bad (
   ip inet PRIMARY KEY NOT NULL,
   peer_ip inet NOT NULL,
   ts int NOT NULL,
-  CONSTRAINT seed_peer_ts UNIQUE (ip,peer_ip,ts)
+  CONSTRAINT ip_bad_seed_peer_ts UNIQUE (ip,peer_ip,ts)
 );
 
 DROP TABLE IF EXISTS networks_good_stats_30 CASCADE;
 CREATE TABLE networks_good_stats_30 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
-  CONSTRAINT network FOREIGN KEY (network) REFERENCES networks (network),
-  CONSTRAINT peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
+  CONSTRAINT networks_good_stats_30_network FOREIGN KEY (network) REFERENCES networks (network),
+  CONSTRAINT networks_good_stats_30_peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
 );
 
 DROP TABLE IF EXISTS networks_good_stats_720 CASCADE;
 CREATE TABLE networks_good_stats_720 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
-  CONSTRAINT network FOREIGN KEY (network) REFERENCES networks (network),
-  CONSTRAINT peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
+  CONSTRAINT networks_good_stats_720_network FOREIGN KEY (network) REFERENCES networks (network),
+  CONSTRAINT networks_good_stats_720_peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
 );
 
 DROP TABLE IF EXISTS networks_bad_stats_30 CASCADE;
 CREATE TABLE networks_bad_stats_30 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
-  CONSTRAINT network FOREIGN KEY (network) REFERENCES networks (network),
-  CONSTRAINT peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
+  CONSTRAINT networks_bad_stats_30_network FOREIGN KEY (network) REFERENCES networks (network),
+  CONSTRAINT networks_bad_stats_30_peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
 );
 
 DROP TABLE IF EXISTS networks_bad_stats_720 CASCADE;
 CREATE TABLE networks_bad_stats_720 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
-  CONSTRAINT network FOREIGN KEY (network) REFERENCES networks (network),
-  CONSTRAINT peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
+  CONSTRAINT networks_bad_stats_720_network FOREIGN KEY (network) REFERENCES networks (network),
+  CONSTRAINT networks_bad_stats_720_peer_network FOREIGN KEY (peer_network) REFERENCES networks (network)
 );
