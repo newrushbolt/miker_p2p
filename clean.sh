d8aae12cfde6d9323f281cd2eea9eb7782aa8afc
Sergey <mihailov.s@outlook.com>
2017-09-12 16:13:43 +0300

    Remove legacy code

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mysql -uroot p2p -e 'truncate table worker_counters;'
-mysql -uroot p2p -e 'truncate table peer_state;'
-mysql -uroot p2p -e 'truncate table peer_lists;'
-ruby rabbit_delete.rb
--- stat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mysql -uroot p2p -e 'select * from worker_counters;'
-mysql -uroot p2p -e 'select count(*) as peers from peer_state;'
-mysql -uroot p2p -e 'select count(*) as lists from peer_lists;'
-mysql -uroot p2p -e 'select count(*) as peer_load_5 from peer_load_5;'
-mysql -uroot p2p -e 'select count(*) as peer_bad_30 from peer_bad_30;'
-sudo rabbitmqctl list_queues
