44f62078dcf15922914a62245375130ed3e027d2
Sergey <mihailov.s@outlook.com>
2017-09-12 17:54:43 +0300

    some ddl debug3

---
+++ init/test_requests.sql
@@ -0,0 +1,5 @@
+insert into channels_slots values ('channel1',10);
+
+insert into peers values 
+('connid1','channel1','ggid1','185.40.152.12'), ('connid2','channel1','ggid2','185.40.152.11'),
+('connid3','channel1','ggid3','185.40.152.10'), ('connid4','channel1','ggid4','185.40.153.12');
