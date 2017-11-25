53884303cddef1af4547fd3e35cbda56d8cdbe75
svmikhailov <svmikhailov@yamoney.ru>
2017-11-25 22:03:43 +0300

    Init sql fix 2

--- init/test.sh
+++ init/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo -u postgres -- psql < gen.sql
-sudo -u postgres -- psql < test_data.sql
-sudo -u postgres -- psql -c "select genereate_peer_list('connid1','channel1');"
+sudo -u postgres -- psql p2p < gen.sql
+sudo -u postgres -- psql p2p < test_data.sql
+sudo -u postgres -- psql p2p -c "select genereate_peer_list('connid1','channel1');"
