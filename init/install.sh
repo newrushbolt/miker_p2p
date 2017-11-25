233b19fcb46dd7920e859dd3291f7cdfae402a1e
svmikhailov <svmikhailov@yamoney.ru>
2017-11-25 22:03:11 +0300

    Init sql fix

--- init/install.sh
+++ init/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo -u postgres -- psql < p2p.sql
+echo -e "\033[0;31mDon't forget to enable password auth for postgres\033[0m\n"
+sudo -u postgres -- psql p2p < p2p.sql
--- init/p2p.sql
+++ init/p2p.sql
@@ -1,9 +1,22 @@
+DROP TABLE IF EXISTS channels_settings CASCADE;
+DROP TABLE IF EXISTS networks CASCADE;
+DROP TABLE IF EXISTS peers CASCADE;
+DROP TABLE IF EXISTS peers_updates CASCADE;
+DROP TABLE IF EXISTS peers_lists CASCADE;
+DROP TABLE IF EXISTS peers_good CASCADE;
+DROP TABLE IF EXISTS ip_good CASCADE;
+DROP TABLE IF EXISTS peers_bad CASCADE;
+DROP TABLE IF EXISTS ip_bad CASCADE;
+DROP TABLE IF EXISTS networks_good_stats_30 CASCADE;
+DROP TABLE IF EXISTS networks_good_stats_720 CASCADE;
+DROP TABLE IF EXISTS networks_bad_stats_30 CASCADE;
+DROP TABLE IF EXISTS networks_bad_stats_720 CASCADE;
+
 -- Добавляет пользователя p2p
 DROP USER IF EXISTS p2p;
-CREATE USER p2p;
+CREATE USER p2p with password 'p2p' login;
 
 -- настройки каналов, обновляет их p2p-сервис
-DROP TABLE IF EXISTS channels_settings CASCADE;
 CREATE TABLE channels_settings (
   channel_id varchar(45) UNIQUE PRIMARY KEY NOT NULL,
   slots_per_seed int NOT NULL,
@@ -12,7 +25,6 @@ CREATE TABLE channels_settings (
 ALTER TABLE channels_settings OWNER to p2p;
 
 -- список сетей с гео и ip информацией, обновляется отдельным приложением
-DROP TABLE IF EXISTS networks CASCADE;
 CREATE TABLE networks (
   network cidr PRIMARY KEY NOT NULL,
   asn int NOT NULL,
@@ -24,7 +36,6 @@ CREATE TABLE networks (
 ALTER TABLE networks OWNER to p2p;
 
 -- онлайн-пиры, обновляются с запросов на подключение, логов, и системой очистки устаревших/зависших пиров
-DROP TABLE IF EXISTS peers CASCADE;
 CREATE TABLE peers (
   conn_id varchar(45) UNIQUE NOT NULL,
   channel_id varchar(45) NOT NULL,
@@ -36,7 +47,6 @@ CREATE TABLE peers (
 ALTER TABLE peers OWNER to p2p;
 
 -- timestamp пиров, обновляются с логов
-DROP TABLE IF EXISTS peers_updates CASCADE;
 CREATE TABLE peers_updates (
   conn_id varchar(45) UNIQUE PRIMARY KEY NOT NULL,
   last_update timestamp NOT NULL,
@@ -45,7 +55,6 @@ CREATE TABLE peers_updates (
 ALTER TABLE peers_updates OWNER to p2p;
 
 -- списки пиров в json, создаются генератором
-DROP TABLE IF EXISTS peers_lists CASCADE;
 CREATE TABLE peers_lists (
   conn_id varchar(45) NOT NULL,
   ts timestamp NOT NULL,
@@ -56,7 +65,6 @@ CREATE TABLE peers_lists (
 ALTER TABLE peers_lists OWNER to p2p;
 
 -- моментальная статистика успешных скачиваний по CONN_ID, обновляется с логов
-DROP TABLE IF EXISTS peers_good CASCADE;
 CREATE TABLE peers_good (
   conn_id varchar(45) NOT NULL,
   peer_conn_id varchar(45) NOT NULL,
@@ -70,7 +78,6 @@ CREATE TABLE peers_good (
 ALTER TABLE peers_good OWNER to p2p;
 
 -- моментальная статистика успешных скачиваний по IP, обновляется с логов
-DROP TABLE IF EXISTS ip_good CASCADE;
 CREATE TABLE ip_good (
   ip inet PRIMARY KEY NOT NULL,
   peer_ip inet NOT NULL,
@@ -82,7 +89,6 @@ CREATE TABLE ip_good (
 ALTER TABLE ip_good OWNER to p2p;
 
 -- моментальная статистика провальных скачиваний по CONN_ID, обновляется с логов
-DROP TABLE IF EXISTS peers_bad CASCADE;
 CREATE TABLE peers_bad (
   conn_id varchar(45) PRIMARY KEY NOT NULL,
   peer_conn_id varchar(45) NOT NULL,
@@ -94,7 +100,6 @@ CREATE TABLE peers_bad (
 ALTER TABLE peers_bad OWNER to p2p;
 
 -- моментальная статистика провальных скачиваний по IP, обновляется с логов
-DROP TABLE IF EXISTS ip_bad CASCADE;
 CREATE TABLE ip_bad (
   ip inet PRIMARY KEY NOT NULL,
   peer_ip inet NOT NULL,
@@ -104,7 +109,6 @@ CREATE TABLE ip_bad (
 ALTER TABLE ip_bad OWNER to p2p;
 
 -- накопительная статистика успешных скачиваний между сетями, выборка за полчаса, обновляется с ip_good
-DROP TABLE IF EXISTS networks_good_stats_30 CASCADE;
 CREATE TABLE networks_good_stats_30 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
@@ -114,7 +118,6 @@ CREATE TABLE networks_good_stats_30 (
 ALTER TABLE networks_good_stats_30 OWNER to p2p;
 
 -- накопительная статистика успешных скачиваний между сетями, выборка за сутки, обновляется с ip_good
-DROP TABLE IF EXISTS networks_good_stats_720 CASCADE;
 CREATE TABLE networks_good_stats_720 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
@@ -124,7 +127,6 @@ CREATE TABLE networks_good_stats_720 (
 ALTER TABLE networks_good_stats_720 OWNER to p2p;
 
 -- накопительная статистика провальных скачиваний между сетями, выборка за полчаса, обновляется с ip_bad
-DROP TABLE IF EXISTS networks_bad_stats_30 CASCADE;
 CREATE TABLE networks_bad_stats_30 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
@@ -134,7 +136,6 @@ CREATE TABLE networks_bad_stats_30 (
 ALTER TABLE networks_bad_stats_30 OWNER to p2p;
 
 -- накопительная статистика провальных скачиваний между сетями, выборка за час, обновляется с ip_bad
-DROP TABLE IF EXISTS networks_bad_stats_720 CASCADE;
 CREATE TABLE networks_bad_stats_720 (
   network cidr PRIMARY KEY NOT NULL,
   peer_network cidr NOT NULL,
