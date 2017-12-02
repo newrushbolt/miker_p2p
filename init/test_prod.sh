2f67c2e2c7c29bb928068b2ee343c6c6a7b65978
svmikhailov <svmikhailov@yamoney.ru>
2017-12-02 13:43:39 +0300

    limit test1.1

--- init/test_prod.sh
+++ init/test_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql p2p -h 127.0.0.1 -p 5432 -Up2p -Wp2p -c "select genereate_peer_list('connid1','channel1',50);"
+psql p2p -h 127.0.0.1 -p 5432 -Up2p -W -c "select genereate_peer_list('connid1','channel1',50);"
